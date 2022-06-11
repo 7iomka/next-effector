9fa2815017236cb7c093bd83cec6602a35768882
7iomka <7iomka@gmail.com>
2022-06-11 21:42:26 +0300

    Remove AppType

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,12 @@ import '@fontsource/acme'
 import '@fontsource/fira-mono'
 import '@/shared/ui/globals.css'
 import type { AppProps } from 'next/app'
-import { AppType } from 'next/dist/shared/lib/utils'
 import { withEffector } from '@/shared/lib/effector'
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout // see next-custom.d.ts
 }
-const MyApp: AppType = ({ Component, pageProps }: AppPropsWithLayout) => {
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   if (Component.getLayout)
     return Component.getLayout(<Component {...pageProps} />)
 
