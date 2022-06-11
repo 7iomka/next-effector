4ccf4a71326643b6ffdcba7674ac237d767328c8
7iomka <7iomka@gmail.com>
2022-06-11 21:48:45 +0300

    Fix typings

--- next-custom.d.ts
+++ next-custom.d.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 /* eslint-disable @typescript-eslint/ban-types */
 import type { NextPage } from 'next'
-import type { ReactElement, ReactNode } from 'react'
+import type { ReactElement } from 'react'
 
 declare global {
-  export declare type NextLayoutGetter = (page: ReactElement) => ReactNode
+  export declare type NextLayoutGetter = (page: ReactElement) => JSX.Element
 
   export declare type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
     getLayout?: NextLayoutGetter
